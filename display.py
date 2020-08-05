8b5de06108a6e5d44a3069fffc000a0becba226e
GrzegorzZmuda <33518770+GrzegorzZmuda@users.noreply.github.com>
2020-08-05 14:59:44 +0200

    gi

--- display.py
+++ display.py
@@ -145,7 +145,7 @@ def disp(data):
             for k in range(3):
                 ls[i][j].append(data[i][j][k])
 
-    fdata = np.array()
+    fdata = np.array(ls)
     surf1 = pygame.surfarray.make_surface(fdata)
     surf1 = pygame.transform.flip(surf1, True, False)
     surf1 = pygame.transform.rotate(surf1, 90)
@@ -230,11 +230,7 @@ def postgresquery(a):
 
 def postgresquerymul(a):
     try:
-            connection = psycopg2.connect(user="postgres",
-                                  password="all",
-                                  host="127.0.0.1",
-                                  port="5432",
-                                  database="API")
+            connection = connectiondata()
             cursor = connection.cursor()
             exstr=a
             cursor.execute(exstr)
